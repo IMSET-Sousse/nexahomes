1e6bc98cacf52b96d0bbdfdfe88ed85218a16f74
elachecheasma <elachecheasma@gmail.com>
2024-01-16 09:16:52 +0100

    initial-commit

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+        <title>NEXAHOMES | NEXT-LEVEL APPROACH TO REAL ESTATE</title>
+
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body>
+        <header class="header-area header-sticky">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <nav class="main-nav">
+                            <a href="index.php" class="logo">
+                                <h1>NEXAHOMES</h1>
+                            </a>
+                            <ul class="nav">
+                                <li><a href="index.php" class="active">Home</a></li>
+                                <li><a href="properties.php">Properties</a></li>
+                                <li><a href="contact.php">Contact Us</a></li>
+                                <li><a href="account.php">Account</a></li>
+                                <li><a href="#"><i class="fa fa-calendar"></i> Schedule a visit</a></li>
+                            </ul>   
+                            <a class='menu-trigger'>
+                                <span>Menu</span>
+                            </a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </body>
+</html>
